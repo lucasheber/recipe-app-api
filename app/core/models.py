5dde2bd8d073082b4ad8b7141594200f73b63524
Lucas Heber <lucas.heber07@gmail.com>
2024-06-19 22:52:31 -0300

    feat: create a new superuser

--- app/core/models.py
+++ app/core/models.py
@@ -15,8 +15,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Crate and return a new superuser"""
+        user = self.create_user(email=email, password=password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
