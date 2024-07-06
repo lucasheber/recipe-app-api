c2696c1e5b67b16862ed6ab7b88429ae57659775
Lucas Heber <lucas.heber07@gmail.com>
2024-07-06 16:12:26 -0300

    lint: fix lint line too long

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,7 +12,14 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'description']
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'link',
+            'description',
+        ]
         read_only_fields = ['id']
 
 
