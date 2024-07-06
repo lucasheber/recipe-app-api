7e77d6ac9247fc185d4741c5070f80f0c02da46c
Lucas Heber <lucas.heber07@gmail.com>
2024-07-06 15:58:18 -0300

    fix: add description in fields recipe serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,7 +12,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'description']
         read_only_fields = ['id']
 
 
