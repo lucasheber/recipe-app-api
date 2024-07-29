594c3b823252e184451c257c16e730e3562bb36d
Lucas Heber <lucas.heber07@gmail.com>
2024-07-29 08:49:13 -0300

    test: test creating Ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -80,3 +80,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEquals(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient'
+        )
+
+        self.assertEquals(str(ingredient), ingredient.name)
