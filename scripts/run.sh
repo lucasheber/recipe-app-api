6bd91a7088d4d65256c74eac59e2d4b2e9b882be
Lucas Heber <lucas.heber07@gmail.com>
2024-08-07 22:26:09 -0300

    fix: correct typo

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@ set -e
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
-python manage.py migrete
+python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --entable-threads --module app.wsgi
